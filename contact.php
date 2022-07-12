c90934e3bffea38717a5d76af61ed6ba667ab93d
thapelozayn <thapelozayndlamini@gmail.com>
2022-07-12 13:46:45 +0100

    updated files

---
+++ contact.php
@@ -0,0 +1,15 @@
+<?php
+if(!empty($_POST["send"])) {
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$subject = $_POST["subject"];
+	$message = $_POST["message"];
+
+	$toEmail = "thapelozayndlamini@gmail.com";
+	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
+	if(mail($toEmail, $subject, $message, $mailHeaders)) {
+	    $message = "Your contact information is received successfully.";
+	    $type = "success";
+	}
+}
+?>
